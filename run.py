e74a6a20d15dbbaeabc8eb9f51ef396a67a5f00a
Sunil D S <sunil3590@gmail.com>
2018-07-11 15:04:18 +0530

    handle invalid input

--- run.py
+++ run.py
@@ -17,31 +17,33 @@ def index():
 
 @app.route("/home",  methods=['GET'])
 def home():
-    user = request.args.get('name')
+    user = request.args.get('name', None)
     return render_template('home.html', user=user)
 
 
 @app.route("/productcreate", methods=['POST'])
 def product_create():
-    product_name = request.form['product_name']
-    product_price = request.form['product_price']
-    if product_name.strip() is None and product_price.strip() is None:
+    product_name = request.form.get('product_name', None)
+    product_price = request.form.get('product_price', None)
+
+    # check input validity
+    if product_name is None and product_price is None:
         return render_template('home.html', error="Product name and price are mandatory")
-    else:
-        try:
-            with sql.connect("database.db") as con:
-                cur = con.cursor()
-                cur.execute("create table if not exists products (name TEXT, price INT)")
-                cur.execute("INSERT INTO products (name,price)"
-                            "VALUES (?,?)", (product_name, product_price))
-                con.commit()
-                msg = "Record saved successfully"
-        except:
-            con.rollback()
-            msg = "Error in inserting record"
-        finally:
-            con.close()
-            return render_template("home.html", msg=msg)
+
+    try:
+        with sql.connect("database.db") as con:
+            cur = con.cursor()
+            cur.execute("create table if not exists products (name TEXT, price INT)")
+            cur.execute("INSERT INTO products (name,price)"
+                        "VALUES (?,?)", (product_name, product_price))
+            con.commit()
+            msg = "Record saved successfully"
+    except:
+        con.rollback()
+        msg = "Error in inserting record"
+    finally:
+        con.close()
+        return render_template("home.html", msg=msg)
 
 
 @app.route("/productlist", methods=['GET'])
